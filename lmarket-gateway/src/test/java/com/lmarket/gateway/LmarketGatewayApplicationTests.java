ff293688b73cce02d3e9949dc5f5c4da45d25d79
branshLEE <1119462481@qq.com>
2021-02-26 22:18:16 +0800

    add gateway module

---
+++ lmarket-gateway/src/test/java/com/lmarket/gateway/LmarketGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmarket.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmarketGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
