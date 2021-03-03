a7b585f9bc5f7197c1a5bbc15ee0b4b3510ce51a
branshLEE <1119462481@qq.com>
2021-03-04 00:24:56 +0800

    add exception class

--- lmarket-product/src/main/java/com/lmarket/product/controller/BrandController.java
+++ lmarket-product/src/main/java/com/lmarket/product/controller/BrandController.java
@@ -1,9 +1,15 @@
 package com.lmarket.product.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
+import com.common.valid.AddGroup;
+import com.common.valid.UpdataGroup;
+import com.common.valid.UpdateStatusGroup;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +21,7 @@ import com.lmarket.product.service.BrandService;
 import com.common.utils.PageUtils;
 import com.common.utils.R;
 
+import javax.validation.Valid;
 
 
 /**
@@ -58,8 +65,23 @@ public class BrandController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("product:brand:save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
+    public R save(@Validated({AddGroup.class}) @RequestBody BrandEntity brand/*, BindingResult result*/){
+//        if(result.hasErrors()){
+//            Map<String, String> map = new HashMap<>();
+//            //获取校验的错误结果
+//            result.getFieldErrors().forEach((item)->{
+//                //获取到错误提示
+//                String message = item.getDefaultMessage();
+//                //获取错误的属性名字
+//                String field = item.getField();
+//                map.put(field, message);
+//            });
+//            return R.error(400, "提交的数据不合法").put("data", map);
+//        }else{
+//            brandService.save(brand);
+//        }
+
+        brandService.save(brand);
 
         return R.ok();
     }
@@ -69,8 +91,16 @@ public class BrandController {
      */
     @RequestMapping("/update")
     //@RequiresPermissions("product:brand:update")
-    public R update(@RequestBody BrandEntity brand){
+    public R update(@Validated(UpdataGroup.class) @RequestBody BrandEntity brand){
 		brandService.updateById(brand);
+
+        return R.ok();
+    }
+
+    @RequestMapping("/update/status")
+    //@RequiresPermissions("product:brand:update")
+    public R updateStatus(@Validated(UpdateStatusGroup.class) @RequestBody BrandEntity brand){
+        brandService.updateById(brand);
 
         return R.ok();
     }
--- lmarket-third-party/src/main/java/com/lmarket/thirdparty/controller/OssController.java
+++ lmarket-third-party/src/main/java/com/lmarket/thirdparty/controller/OssController.java
@@ -4,6 +4,7 @@ import com.aliyun.oss.OSS;
 import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
+import com.common.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +28,7 @@ public class OssController {
     private String accessId;
 
     @RequestMapping("/oss/policy")
-    public Map<String, String> policy(){
+    public R policy(){
 
         String bucket = "lmarket"; // 请填写您的 bucketname 。
         String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
@@ -68,6 +69,6 @@ public class OssController {
             ossClient.shutdown();
         }
 
-        return respMap;
+        return R.ok().put("data", respMap);
     }
 }
