5ddf05c3ce78381ba2d0af582f300a01eaa0480d
branshLEE <1119462481@qq.com>
2021-03-14 17:49:37 +0800

    add ware-sku search interface

--- lmarket-ware/src/main/java/com/lmarket/ware/LmarketWareApplication.java
+++ lmarket-ware/src/main/java/com/lmarket/ware/LmarketWareApplication.java
@@ -1,8 +1,14 @@
 package com.lmarket.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@MapperScan("com.lmarket.ware.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LmarketWareApplication {
 
--- lmarket-ware/src/main/java/com/lmarket/ware/service/impl/WareSkuServiceImpl.java
+++ lmarket-ware/src/main/java/com/lmarket/ware/service/impl/WareSkuServiceImpl.java
@@ -11,6 +11,7 @@ import com.common.utils.Query;
 import com.lmarket.ware.dao.WareSkuDao;
 import com.lmarket.ware.entity.WareSkuEntity;
 import com.lmarket.ware.service.WareSkuService;
+import org.springframework.util.StringUtils;
 
 
 @Service("wareSkuService")
@@ -18,9 +19,21 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        QueryWrapper<WareSkuEntity> queryWrapper = new QueryWrapper<>();
+        String skuId = (String) params.get("skuId");
+        if(!StringUtils.isEmpty(skuId)){
+            queryWrapper.eq("sku_id", skuId);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if(!StringUtils.isEmpty(wareId)){
+            queryWrapper.eq("ware_id", wareId);
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
