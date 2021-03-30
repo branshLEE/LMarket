5d16471b20bc6e5f3e2f2b3ad621727abc10394d
branshLEE <1119462481@qq.com>
2021-03-30 10:45:23 +0800

    pageNums jumping and param replace configuration.

--- lmarket-search/src/main/java/com/lmarket/search/constant/EsConstant.java
+++ lmarket-search/src/main/java/com/lmarket/search/constant/EsConstant.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 public class EsConstant {
     public static final String PRODUCT_INDEX = "lmarket_product"; //sku数据在es中的索引
-    public static final Integer PRODUCT_PAGESIZE = 100;
+    public static final Integer PRODUCT_PAGESIZE = 3;
 }
