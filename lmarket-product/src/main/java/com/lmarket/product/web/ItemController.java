b778f2b451fa30e7f1e1c2267e6ac6cc095871bf
branshLEE <1119462481@qq.com>
2021-04-09 12:23:36 +0800

    add item.html page.

---
+++ lmarket-product/src/main/java/com/lmarket/product/web/ItemController.java
@@ -0,0 +1,20 @@
+package com.lmarket.product.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ItemController {
+
+    /**
+     * 展示sku的详情
+     * @param skuId
+     * @return
+     */
+    @GetMapping("/{skuId}.html")
+    public String skuItem(Long skuId){
+
+        System.out.println("准备查询"+skuId+"详情");
+        return "item";
+    }
+}
