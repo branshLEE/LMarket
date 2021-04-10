ab30530468bb835213f8134fb665af77a7ca7a9f
branshLEE <1119462481@qq.com>
2021-04-11 01:08:16 +0800

    add module of lmarket-auth-server.

---
+++ lmarket-auth-server/src/test/java/com/lmarket/auth/LmarketAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmarket.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmarketAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
