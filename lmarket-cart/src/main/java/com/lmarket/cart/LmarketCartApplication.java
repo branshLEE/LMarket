e9654e73205f7450be856376b0c02c6942a3b599
branshLEE <1119462481@qq.com>
2021-04-15 18:44:19 +0800

    add module of lmarket-cart.

---
+++ lmarket-cart/src/main/java/com/lmarket/cart/LmarketCartApplication.java
@@ -0,0 +1,18 @@
+package com.lmarket.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class LmarketCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmarketCartApplication.class, args);
+    }
+
+}
---
+++ lmarket-cart/src/test/java/com/lmarket/cart/LmarketCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmarket.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmarketCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
