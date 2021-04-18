05d31ccefe9a539b2bbb7341479bbcdcc330ca84
branshLEE <1119462481@qq.com>
2021-04-18 18:20:28 +0800

    order pages for rending.

--- lmarket-order/src/main/java/com/lmarket/order/LmarketOrderApplication.java
+++ lmarket-order/src/main/java/com/lmarket/order/LmarketOrderApplication.java
@@ -3,12 +3,16 @@ package com.lmarket.order;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * 使用RabbitMQ
  * 1、引入amqp场景
  */
 
+@EnableRedisHttpSession
+@EnableDiscoveryClient
 @EnableRabbit
 @SpringBootApplication
 public class LmarketOrderApplication {
