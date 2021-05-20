ab1e2a138b8c351b341789c2c228cd085853fcdd
branshLEE <1119462481@qq.com>
2021-05-20 16:18:30 +0800

    reset redisson's url.

--- lmarket-product/src/main/java/com/lmarket/product/config/MyRedissonConfig.java
+++ lmarket-product/src/main/java/com/lmarket/product/config/MyRedissonConfig.java
@@ -3,6 +3,7 @@ package com.lmarket.product.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,7 +21,7 @@ public class MyRedissonConfig {
     RedissonClient redisson() throws IOException {
         //1.创建配置
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://192.168.56.10:6379");
+        config.useSingleServer().setAddress("redis://redis-service.lmarket:6379");
         //2.根据Config创建出RedissonClien实例
         RedissonClient redissonClient = Redisson.create(config);
         return redissonClient;
