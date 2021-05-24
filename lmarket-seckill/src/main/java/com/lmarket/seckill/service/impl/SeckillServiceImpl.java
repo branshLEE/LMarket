a02567e9d84099e48180ed54292135f3bd54d8b5
branshLEE <1119462481@qq.com>
2021-05-25 01:54:57 +0800

    remove mybatis-plus from seckill

--- lmarket-seckill/src/main/java/com/lmarket/seckill/service/impl/SeckillServiceImpl.java
+++ lmarket-seckill/src/main/java/com/lmarket/seckill/service/impl/SeckillServiceImpl.java
@@ -6,7 +6,7 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
-import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+//import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.common.id.SnowFlakeGenerateIdWorker;
 import com.common.to.SeckillOrderTo;
 import com.common.to.SkuReductionTo;
