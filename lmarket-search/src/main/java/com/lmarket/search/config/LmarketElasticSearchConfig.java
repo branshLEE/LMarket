c2f2b4fd5e6d728140b8a5c6422bee76ed28340d
branshLEE <1119462481@qq.com>
2021-05-24 16:39:58 +0800

    reset es's url.

--- lmarket-search/src/main/java/com/lmarket/search/config/LmarketElasticSearchConfig.java
+++ lmarket-search/src/main/java/com/lmarket/search/config/LmarketElasticSearchConfig.java
@@ -26,13 +26,10 @@ public class LmarketElasticSearchConfig {
 
         RestClientBuilder builder = null;
 
-        builder = RestClient.builder(new HttpHost("192.168.56.10", 9200, "http"));
+        builder = RestClient.builder(new HttpHost("elasticsearch.lmarket", 9200, "http"));
 
         RestHighLevelClient client = new RestHighLevelClient(builder);
 
-//        RestHighLevelClient client = new RestHighLevelClient(
-//                RestClient.builder(
-//                        new HttpHost("192.168.56.10", 9200, "http")
         return client;
     }
 }
