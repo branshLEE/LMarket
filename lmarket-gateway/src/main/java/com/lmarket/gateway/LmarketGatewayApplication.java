55febde46eb44f64856e32551a325671c9fcab82
branshLEE <1119462481@qq.com>
2021-05-24 17:19:20 +0800

    reset pom.xml of gateway.

--- lmarket-gateway/src/main/java/com/lmarket/gateway/LmarketGatewayApplication.java
+++ lmarket-gateway/src/main/java/com/lmarket/gateway/LmarketGatewayApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class LmarketGatewayApplication {
 
     public static void main(String[] args) {
